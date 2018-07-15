15d7cad65a4ef05af3deb25c9f4c1b86addc5ceb
Norberto Ibarra <ibarra.plascencia@gmail.com>
2018-07-15 01:28:27 -0500

    Create CustomerController

--- src/main/java/com/bolsadeideas/springboot/app/controllers/CustomerController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/CustomerController.java
@@ -1,14 +1,26 @@
 package com.bolsadeideas.springboot.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bolsadeideas.springboot.app.models.dao.ICustomerDao;
 
 @Controller
 public class CustomerController {
 
+	@Autowired
+	@Qualifier("customerDaoJPA") //Requerido solo cuando más de un JPA implementa la misma interfaz
+	private ICustomerDao customerDao;
+	
+	@RequestMapping(name="customers", method=RequestMethod.GET)
 	public String index(Model model) {
 		
 		model.addAttribute("title", "All customers");
+		model.addAttribute("customers", customerDao.findAll());
 		
 		return "list";
 	}
--- src/main/java/com/bolsadeideas/springboot/app/models/dao/CustomerDaoImpl.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/dao/CustomerDaoImpl.java
@@ -11,16 +11,16 @@ import org.springframework.transaction.annotation.Transactional;
 import com.bolsadeideas.springboot.app.models.entity.Customer;
 
 
-@Repository
+@Repository("customerDaoJPA") //Especifica nombre de JPA (Para cuando más de una clase implementa la misma interfaz DAO)
 public class CustomerDaoImpl implements ICustomerDao {
 	
 	@PersistenceContext
 	private EntityManager entityManager;
 
+	@SuppressWarnings("unchecked")
 	@Transactional(readOnly=true)
 	@Override
 	public List<Customer> findAll() {
-		// TODO Auto-generated method stub
 		return entityManager.createQuery("from CUSTOMERS").getResultList();
 	}
 
